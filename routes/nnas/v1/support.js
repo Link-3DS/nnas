dd517da1294d3b7c328cc2e44f8be00323d0da80
Raptor06 <137603383+rraptor06@users.noreply.github.com>
2025-06-23 18:44:37 +0200

    Update support.js

--- routes/nnas/v1/support.js
+++ routes/nnas/v1/support.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/validate/email', async (req, res) => {
     const { email } = req.body;
 
-    const domain = String(email).substring(email.lastIndexOf("@") + 1);
+    const domain = String(email).substring(email.lastIndexOf('@') + 1);
 
     try {
         const ping = child_process.execSync(`ping -c 1 ${domain}`, {
